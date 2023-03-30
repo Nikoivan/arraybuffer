961712fe3d450463acf50a9b289a5916748931ff
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-03-30 22:34:34 +0300

    task had been corrected

--- test/test.js
+++ test/test.js
@@ -1,16 +1,15 @@
-import ArrayBufferConverter from "../src/js/app";
-import { getBuffer } from "../src/js/app";
+import ArrayBufferConverter, { getBuffer } from '../src/js/app';
 
 const arr = new ArrayBufferConverter();
 
 arr.load(getBuffer());
 
-test("test for method load of class ArrayBufferConverter", () => {
+test('test for method load of class ArrayBufferConverter', () => {
   const result = arr.code.length;
   expect(result).toBe(53);
 });
 
-test("test for method toString of class ArrayBufferConverter", () => {
+test('test for method toString of class ArrayBufferConverter', () => {
   const result = arr.toString();
   expect(result).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
 });
